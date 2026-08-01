355a30bab2aa83d1106737846c61ca9380babca0
8782gsmwt6-hue <8782gsmwt6@privaterelay.appleid.com>
2026-08-01 07:01:06 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,10 @@
-firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC6nXX0G3CUuvtW_CNW7mg26wOFePc8N_0",
   authDomain: "addie-enterprises.firebaseapp.com",
   projectId: "addie-enterprises",
   storageBucket: "addie-enterprises.firebasestorage.app",
   messagingSenderId: "994123118082",
   appId: "1:994123118082:web:81c056a07fc166ae6e991b"
+};
+
+export const WORKSPACE_ID = "addie-enterprises";
